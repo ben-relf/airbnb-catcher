79b0d137f4cd99636ff0cc3b9ae49679f8ce6833
Ben Relf <bjrelf@gmail.com>
2020-03-14 22:13:47 +1100

    Create airbnb-catcher.py

---
+++ airbnb-catcher.py
@@ -0,0 +1,16 @@
+def main():
+
+    print("Job started")
+
+    try:
+        print("Trying")
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        print("All finished")
+
+
+if __name__ == '__main__':
+    main()
